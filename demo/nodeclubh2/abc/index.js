3b5394cdaa88c47911e71e3611b28c6cfa61a296
1000copy <1000copy@gmail.com>
2016-06-21 17:47:17 +0800

    ajax to server get json

--- demo/nodeclubh2/abc/index.js
+++ demo/nodeclubh2/abc/index.js
@@ -17,6 +17,18 @@ server.register(Inert, () => {});
 
 server.route({
     method: 'GET',
+    path: '/ajax/topics',
+    handler: function (request, reply) {
+        var url ="./img/88caadc211781b5ba82d07e435443213"
+        reply([
+               { title: 'Learn JavaScript' ,replys : 1111,visits:3,avatar:url},
+               { title: 'Learn Vue.js' ,replys : 2,visits:2,avatar:url},
+               { title: 'Build Something Awesome' ,replys : 3,visits:1,avatar:url}
+             ]);
+    }
+});
+server.route({
+    method: 'GET',
     path: '/{param*}',
     handler: {
         directory: {
